3fe6f529aba10cec5680f679760aea623381991d
chinhpv95 <chinhpv95@gmail.com>
2016-07-18 17:19:25 +0700

    Update UI

--- application/views/site/body.php
+++ application/views/site/body.php
@@ -30,31 +30,28 @@
 							<a class="caption button-radius" href="#"><span class="icon"></span>Mua ngay</a>
 						</div>
 					</li>
-					<li><img src="img/h4-slide.png" alt="Slide">
+					<li><img src="upload/content/h3-slide.png" alt="Slide">
 						<div class="caption-group">
 							<h2 class="caption title">
-								by one, get one <span class="primary">50% <strong>off</strong></span>
+								Bí mật đế chế <span class="primary"><strong>Angkor</strong></span>
 							</h2>
-							<h4 class="caption subtitle">school supplies & backpacks.*</h4>
 							<a class="caption button-radius" href="#"><span class="icon"></span>Mua ngay</a>
 						</div>
 					</li>
-					<li><img src="img/h4-slide.png" alt="Slide">
+					<li><img src="upload/content/h2-slide.png" alt="Slide">
 						<div class="caption-group">
 							<h2 class="caption title">
-								Apple <span class="primary">Store <strong>Ipod</strong></span>
+								Lụa <span class="primary"> <strong>Angkor</strong></span>
 							</h2>
-							<h4 class="caption subtitle">Select Item</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Mua ngay</a>
 						</div>
 					</li>
-					<li><img src="img/h4-slide.png" alt="Slide">
+					<li><img src="upload/content/h1-slide.png" alt="Slide">
 						<div class="caption-group">
 						  <h2 class="caption title">
-								Apple <span class="primary">Store <strong>Ipod</strong></span>
+								Bức tranh <span class="primary">làng quê <strong>Campuchia</strong></span>
 							</h2>
-							<h4 class="caption subtitle">& Phone</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Shop now</a>
+							<a class="caption button-radius" href="#"><span class="icon"></span>Mua ngay</a>
 						</div>
 					</li>
 				</ul>
